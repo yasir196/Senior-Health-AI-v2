a1f171b69417bd0166a19b820fcdea8aaad8d754
yasir196 <yasir196@gmail.com>
2026-09-25 20:14:15 +0500

    feat(thumbnail): expose provider contract

---
+++ Thumbnail_Pipeline/providers/__init__.py
@@ -0,0 +1,3 @@
+from .contract import ImageGenerationBackend, ContractRendererProvider, build_provider_request
+
+__all__ = ["ImageGenerationBackend", "ContractRendererProvider", "build_provider_request"]
