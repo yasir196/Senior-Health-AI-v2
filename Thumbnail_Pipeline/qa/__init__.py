a5d62f0e932d0289ebe8887a63835951dd786303
yasir196 <yasir196@gmail.com>
2026-09-25 20:02:36 +0500

    feat(thumbnail): expose post-render QA gate

--- Thumbnail_Pipeline/qa/__init__.py
+++ Thumbnail_Pipeline/qa/__init__.py
@@ -1,3 +1,6 @@
 from .engine import apply_composition_review, evaluate_generation_gate
 
 __all__ = ["apply_composition_review", "evaluate_generation_gate"]
+from .post_render import evaluate_post_render_qa, apply_post_render_review
+
+__all__ = ["apply_composition_review", "evaluate_generation_gate", "evaluate_post_render_qa", "apply_post_render_review"]
