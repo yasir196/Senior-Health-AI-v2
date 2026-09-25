9ee302742f6a1224ba412b0b128e3679a94894c6
yasir196 <yasir196@gmail.com>
2026-09-25 20:47:03 +0500

    feat(thumbnail): expose visual QA provider boundary

--- Thumbnail_Pipeline/qa/__init__.py
+++ Thumbnail_Pipeline/qa/__init__.py
@@ -4,3 +4,4 @@ __all__ = ["apply_composition_review", "evaluate_generation_gate"]
 from .post_render import evaluate_post_render_qa, apply_post_render_review
 
 __all__ = ["apply_composition_review", "evaluate_generation_gate", "evaluate_post_render_qa", "apply_post_render_review"]
+from .visual_provider import VisualQAProvider, build_visual_qa_request, inspect_visual_qa
